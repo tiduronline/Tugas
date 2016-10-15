ac75fa6112961fae34735c677a3b836f5dc58695
Verri A <verri@tiduronline.com>
2016-10-15 08:39:29 +0700

    Other Version of my java homework

--- java/Menghitung.java
+++ java/Menghitung.java
@@ -6,21 +6,66 @@
  * Dosen: A. Sidiq Purnomo
  * MatKul: Pemograman Java
  *
- * Note: Copyleft version >:D
  * @author Verri Andriawan <verri@tiduronline.com>
- *
- * 
  */
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
+
+/**
+ * Static Banner
+ * ============================================== */
+
+class Banner {
+
+	private static String[] biodata = {
+		"// ------------------------------------",
+		"//  Nama\t: Verri Andriawan ",
+		"//  NIM \t: 14111036 ",
+		"// ------------------------------------"
+	};
+
+
+	private static String[] titleProgram = {
+		"===== Program Menghitung Luas =====",
+		"  1. Menghitung luas persegi panjang",
+		"  2. Menghitung luas persegi",
+		"  3. Input Biodata",
+		"  4. Selesai",
+		"===================================",
+	};
+
+	private static String arrayJoin(String[] text) {
+		String sBanner = "\n";
+		
+		for(String txt: text) {
+			sBanner += txt;
+			sBanner += "\n";
+		};
+		return sBanner += "\n";
+	}
+
+	public static void print(String type) {
+
+		switch(type) {
+			case "biodata":
+				String sBannerIdentity = arrayJoin(Banner.biodata);	
+				System.out.print(sBannerIdentity);
+				break;
+			case "title":
+				String sBannerChoice = arrayJoin(Banner.titleProgram);
+				System.out.print(sBannerChoice);
+		}
+	}
+
+}
 
 
 /**
  * Main Math Class
  * ============================================== */
 
-class Hitung {
+class HitungObject {
 
 	protected float lebar = 0;
 	protected float panjang = 0;
@@ -38,78 +83,101 @@ class Hitung {
 
 
 /**
+ * Validation Class
+ * ============================================== */
+
+class Validation {
+    HitungObject obj;
+
+	public Validation(HitungObject obj) {
+		this.obj = obj;
+	}
+
+	public Boolean is_equal() {
+		if(obj.lebar == obj.panjang) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Boolean precision(){
+		if(obj.lebar < obj.panjang)
+			return true;
+		return false;
+	}
+
+
+	public Boolean positive() {
+		if ( obj.lebar < 0 || 
+			 obj.panjang < 0 ||
+			 obj.sisi < 0
+			) 
+		{ return false; }
+
+		return true;
+	}
+}
+
+
+
+/**
  * Main Class
  * ============================================== */
 
 class Menghitung {
 	public static void main(String[] args) {
-		System.out.println("// ------------------------------------");
-		System.out.println("//  Nama\t: Verri A ");
-		System.out.println("//  NIM \t: 14111*** ");
-		System.out.println("// ------------------------------------\n\n");
-
-		float hasil;
-		float lebar;
-		float panjang;
-
-		String nim;
-		String nama;
-		String mikir; 
-
+		Banner.print("biodata");
+		
+		float hasil = 0, lebar = 0, panjang = 0;
+		String nim, nama, mikir; 
 		Boolean loop = false;
 		
-		Hitung hitung = new Hitung();
-		
+
+		HitungObject hitung_object = new HitungObject();
+		Validation validation = new Validation(hitung_object);
 		
 
 		while(!loop) {
-			System.out.println("===== Program Menghitung Luas =====");
-			System.out.println("  1. Menghitung luas persegi panjang");
-			System.out.println("  2. Menghitung luas persegi");
-			System.out.println("  3. Input Biodata");
-			System.out.println("  4. Selesai");
-			System.out.println("===================================");
+			Banner.print("title");
+			pr("Pilihan ( 1-4 ) ? ");
 
-			System.out.print("Pilihan ( 1-4 ) ? ");
-
-			int pilih;
-			int error;
-			Scanner in = new Scanner(System.in);
+			int choiced;
+			Boolean error = true;
+			Scanner inputReader = new Scanner(System.in);
 			String data;
 
-			pilih = in.nextInt();
+			choiced = (int)get_value_or_loop(inputReader, "Hanya ada pilihan 1 - 4. Silahkan masukan pilihan anda ");
 			
 
-			switch(pilih) {
+			switch(choiced) {
 
 				/**
  				 * Hitung Persegi Panjang
 				 * ======================================= */
 				case 1:
-					System.out.println("****** Menghitung Luas Bangun Persegi Panjang ******");
-					error = 1;
+					prln("****** Menghitung Luas Bangun Persegi Panjang ******");
 					
-					while(error > 0) {
-						System.out.print("Panjang Persegi Panjang = ");
-						hitung.panjang = in.nextFloat();
+					while(error) {
+						pr("Panjang Persegi Panjang = ");
+						hitung_object.panjang = get_value_or_loop(inputReader, "Hanya diperbolehkan masukan angka positive. ");
 
-						System.out.print("Lebar Persegi Panjang = ");
-						hitung.lebar = in.nextFloat();
+						pr("Lebar Persegi Panjang = ");
+						hitung_object.lebar = get_value_or_loop(inputReader, "Hanya diperbolehkan masukan angka positive. ");
 
-						if( hitung.panjang > 0 && hitung.lebar > 0) {
+						if (validation.positive()) {
+							if (validation.precision() && !validation.is_equal()) {
 
-							if( hitung.lebar < hitung.panjang ) {
-								hasil = hitung.persegi_panjang();
-								error = 0;
-								System.out.println("Luas persegi panjang = " + hasil );
+								hasil 	= hitung_object.persegi_panjang();
+								error 	= false;
+
+								prln("\n[+] Luas Persegi Panjang adalah " + hasil + "\n");
+
 							} else {
-								System.out.println("Lebar harus lebih kecil dari panjang");
+								prln("\n[E] Ukuran panjang harus lebih besar dari lebar \n");
 							}
-	
 						} else {
-							System.out.println("Nilai lebar dan panjanga tidak boleh negative");
+							prln("\n[E] Ukuran tidak boleh kurang dari 0 \n");
 						}
-
 					}
 					break;
 
@@ -118,51 +186,78 @@ class Menghitung {
  				 * Hitung Persegi
 				 * ======================================= */
 				case 2:
-					System.out.println("****** Menghitung Luas Bangun Persegi ******");
-					error = 1;
+					prln("****** Menghitung Luas Bangun Persegi ******");
 
-					while(error > 0) {
-						System.out.print("Panjang Persegi Panjang = ");
-						hitung.sisi = in.nextFloat();
+					while(error) {
+						pr("Panjang Persegi Panjang = ");
+						hitung_object.sisi = get_value_or_loop(inputReader, "Hanya diperbolehkan masukan angka positive. ");
+						
+						if(validation.positive()) {
+							
+							hasil 	= hitung_object.persegi();
+							error 	= false;
 
-						if(hitung.sisi > 0) {
-							hasil = hitung.persegi();
-							error = 0;
-							System.out.println("Luas Persegi adalah " + hasil);
+							prln("\n[+] Luas Persegi adalah " + hasil + "\n");
 						} else {
-							System.out.println("tidak boleh kurang dari 0");
+							prln("\n[E] Ukuran tidak boleh kurang dari 0 \n");
 						}
 					}
 
 					break;
 				case 3:
-					System.out.println("****** Biodata ******");
+					prln("****** Biodata ******");
 					
-					System.out.print("NIM: ");
-					nim = in.next();
+					pr("NIM: ");
+					nim = inputReader.next();
 				
-					System.out.print("Nama: ");
-					nama = in.next();
+					pr("Nama: ");
+					nama = inputReader.next();
 
-					System.out.print("Apa yang anda sedang pikirkan? ");
-					mikir = in.next();
+					pr("Apa yang anda sedang pikirkan? ");
+					mikir = inputReader.next();
 
-					System.out.println("\n****** Biodata ******");
-					System.out.println("NIM\t: "+nim);
-					System.out.println("Nama\t: "+nama);
-					System.out.println("Saya sedang memikirkan = " + mikir);
+					prln("\n****** Biodata ******");
+					prln("NIM\t: "+nim);
+					prln("Nama\t: "+nama);
+					prln("Saya sedang memikirkan = " + mikir);
 					
 					break;
 				case 4:
 					loop = true;
-					System.out.println("Program terminated");
+					prln("Program terminated");
 					break;
 				default:
 					loop = true;
-					System.out.println("Nilai undefined");
+					prln("Nilai undefined");
 
 			}
 		}
 	}
 
+	private static float get_value_or_loop(Scanner inputReader, String message) {
+		Boolean input_error = true;
+		float value = 0;
+		
+		while(input_error) {
+			if(inputReader.hasNextLine()){
+				try {
+					value = inputReader.nextFloat();	
+					input_error = false;
+				} catch(InputMismatchException e) {
+					prln(message);
+					pr("=> ");
+					inputReader.next();
+				}
+			} 
+		}
+		return value;
+	}
+
+	private static void pr(String message) {
+		System.out.print(message);
+	}
+
+	private static void prln(String message) {
+		System.out.println(message);
+	}
 }
